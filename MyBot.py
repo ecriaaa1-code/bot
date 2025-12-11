49200309d7c6570c83c0330d6c4bd30ba508b8be
ecriaaa1-code <ecriaaa1@gmail.com>
2025-12-11 18:56:10 +0530

    Update MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -3,15 +3,13 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
-import asyncio
-from collections import deque
 
-from keep_alive import keep_alive  # NEW
+from keep_alive import keep_alive
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-keep_alive()  # NEW
+keep_alive()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,18 +21,13 @@ async def on_ready():
     await bot.tree.sync()
     print(f"{bot.user} is online!")
 
-
 @bot.event
 async def on_message(msg):
-    # ignore the bot's own messages
     if msg.author == bot.user:
         return
-
-    # simple test reply
+    
     await msg.channel.send("Interesting message!")
-
-    # make sure commands still work
     await bot.process_commands(msg)
 
-
 bot.run(TOKEN)
+
